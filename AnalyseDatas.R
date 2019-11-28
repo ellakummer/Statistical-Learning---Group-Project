ab4b0e8245ed77119b0daffb2e4f8921981794dc
Ella Kummer <ella.kummer@etu.unige.ch>
2019-11-28 16:48:01 +0100

    improvements

--- AnalyseDatas.R
+++ AnalyseDatas.R
@@ -27,7 +27,7 @@ glm.fit=glm(target~age+sex+cp+trestbps+chol+fbs+restecg+thalach+exang+oldpeak+sl
 coef(glm.fit)
 summary(glm.fit)
 
-# test (test error MSE) :
+# test error MSE :
 mean((target-predict(glm.fit,my_data, type="response"))[-train]^2)
 
 # CROSS VALIDATION : 
@@ -43,15 +43,65 @@ cv.error.10
 cv.error=cv.glm(my_data,glm.fit2)$delta[1]
 cv.error
 
+# ----------- TREE BASED METHOD  -----------
+
+#install.packages("tree")
+# CLASSIFICATION  tree : 
+library(tree)
+disease=ifelse(target<=0.5,"No","Yes")
+my_data_2=data.frame(my_data,disease)
+tree.datas=tree(disease~.-target,my_data_2)
+summary(tree.datas)
+plot(tree.datas)
+text(tree.datas,pretty=0)
+tree.datas
+
+set.seed(2)
+train=sample(1:nrow(my_data_2), 150)
+my_data_2.test=my_data_2[-train,]
+disease.test=disease[-train]
+tree.datas=tree(disease~.-target,my_data_2,subset=train)
+tree.pred=predict(tree.datas,my_data_2,type="class")
+table(tree.pred,disease.test)
+(104+50)/200
+
+# PRUNNING TREE 
+set.seed(3)
+cv.prune.datas=cv.tree(tree.datas,FUN=prune.misclass)
+cv.prune.datas
+par(mfrow=c(1,2))
+prune.datas=prune.misclass(tree.datas,best=4)
+plot(prune.datas)
+text(prune.datas,pretty=0)
+# check error 
+tree.pred=predict(prune.datas,Carseats.test,type="class")
+table(tree.pred,High.test)
+(94+60)/200
+prune.carseats=prune.misclass(tree.carseats,best=15)
+plot(prune.carseats)
+text(prune.carseats,pretty=0)
+tree.pred=predict(prune.carseats,Carseats.test,type="class")
+table(tree.pred,High.test)
+(86+62)/200
+
+
+# ---------------------------------------------------------------
+# ------------------ BEST SUBSET SELECTION  ---------------------
+# ---------------------------------------------------------------
+
+# with the result : try non-linear method (with lm.())
+
+
+
+# ---------------------------------------------------------------
+# ---------------------NON LINEAR MODELING ----------------------
+# ---------------------------------------------------------------
 
 # ----------- POLYNOMIAL REGRESSION + SPLINE -----------
 
+# see tuto5 !! :) 
 
 
-# ----------- TREE BASED METHOD  -----------
 
-
-# ----------- BEST SUBSET SELECTION  -----------
-# with the result : try non-linear method (with lm.())
 
 
