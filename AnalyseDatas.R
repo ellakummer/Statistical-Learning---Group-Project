e2a7d13cdbd85db5f579382180a3056b9c20cb83
Ella Kummer <ella.kummer@etu.unige.ch>
2019-11-28 17:41:10 +0100

    bests subsets

--- AnalyseDatas.R
+++ AnalyseDatas.R
@@ -65,7 +65,7 @@ disease.test=disease[-train]
 tree.datas=tree(disease~.-target,my_data_2,subset=train)
 tree.pred=predict(tree.datas,my_data_2.test,type="class")
 table(tree.pred,disease.test)
-# (49+64)/(49+22+18+64) = 113/153 = 0.7386
+# (52+64)/(49+22+18+64) = 116/153 = 0.7582
 
 # PRUNNING TREE 
 set.seed(3)
@@ -85,6 +85,102 @@ table(tree.pred2,disease.test)
 # ---------------------------------------------------------------
 
 # with the result : try non-linear method (with lm.())
+
+install.packages("leaps")
+library(leaps)
+
+regfit.full=regsubsets(target~.,data=my_data,nvmax=13)
+# to predict the salary, we found these predictors in the Hitters components set:
+reg.summary=summary (regfit.full) 
+reg.summary
+
+# models:
+names(reg.summary)
+
+# USELESS ?? 
+# we want according to adjusted R2, BIC, Cp
+reg.summary$adjr2
+reg.summary$bic
+reg.summary$cp
+reg.summary$rsq
+
+par(mfrow=c(2,2))
+
+# USELESS ?? 
+plot(reg.summary$rss,xlab="Number of Variables",ylab="RSS",type="l")
+
+# bests models according to : 
+
+plot(reg.summary$adjr2,xlab="Number of Variables",ylab="Adjusted RSq",type="l")
+which.max(reg.summary$adjr2)
+points(10,reg.summary$adjr2[10], col="red",cex=2,pch=20)
+coef(regfit.full ,10)
+
+plot(reg.summary$cp,xlab="Number of Variables",ylab="Cp",type='l')
+which.min(reg.summary$cp)
+points(9,reg.summary$cp[9],col="red",cex=2,pch=20)
+coef(regfit.full ,9)
+
+plot(reg.summary$bic,xlab="Number of Variables",ylab="BIC",type='l')
+which.min(reg.summary$bic)
+points(7,reg.summary$bic[7],col="red",cex=2,pch=20)
+coef(regfit.full ,7)
+
+# Show some plots to provide evidence : 
+# USELESS ??  don't like how it's plot 
+plot(regfit.full,scale="r2")
+plot(regfit.full,scale="adjr2")
+plot(regfit.full,scale="Cp")
+plot(regfit.full,scale="bic")
+
+# Choosing Among Models
+
+set.seed(1)
+train=sample(c(TRUE,FALSE), nrow(Hitters),rep=TRUE)
+test=(!train)
+regfit.best=regsubsets(Salary~.,data=Hitters[train,],nvmax=19)
+test.mat=model.matrix(Salary~.,data=Hitters[test,])
+val.errors=rep(NA,19)
+test.mat
+for(i in 1:19){
+  coefi=coef(regfit.best,id=i)
+  pred=test.mat[,names(coefi)]%*%coefi
+  val.errors[i]=mean((Hitters$Salary[test]-pred)^2)
+}
+val.errors
+which.min(val.errors)
+coef(regfit.best,10)
+
+predict.regsubsets=function(object,newdata,id,...){
+  form=as.formula(object$call[[2]])
+  mat=model.matrix(form,newdata)
+  coefi=coef(object,id=id)
+  xvars=names(coefi)
+  mat[,xvars]%*%coefi
+}
+regfit.best=regsubsets(Salary~.,data=Hitters,nvmax=19)
+coef(regfit.best,10)
+k=10
+set.seed(1)
+folds=sample(1:k,nrow(Hitters),replace=TRUE)
+cv.errors=matrix(NA,k,19, dimnames=list(NULL, paste(1:19)))
+
+
+for(j in 1:k){
+  best.fit=regsubsets(Salary~.,data=Hitters[folds!=j,],nvmax=19)
+  for(i in 1:19){
+    pred=predict(best.fit,Hitters[folds==j,],id=i)
+    cv.errors[j,i]=mean( (Hitters$Salary[folds==j]-pred)^2)
+  }
+}
+
+mean.cv.errors=apply(cv.errors,2,mean)
+mean.cv.errors
+which.min(mean.cv.errors)
+par(mfrow=c(1,1))
+plot(mean.cv.errors,type='b')
+reg.best=regsubsets(Salary~.,data=Hitters, nvmax=19)
+coef(reg.best,11)
 
 
 
